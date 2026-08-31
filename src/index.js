74b1c8c0e4b06909bbfc34e93527c17480147ef7
omyadav786 <ayurashiyadav143@gmail.com>
2026-08-31 18:14:54 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -16,15 +16,19 @@ import dotenv from "dotenv";
 dotenv.config({
     path: "./.env"
 })
-connectDb();
 
 
+connectDb()
 
+.then(() =>{
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`Server started on port ${process.env.PORT || 8000}`);
+    })
+})
 
-
-
-
-
+.catch((error) =>{
+    console.log("Error from DB connection" , error);
+})
 
 
 // const {DB_name} = require("./contants.js");
